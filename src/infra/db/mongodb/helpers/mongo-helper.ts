4fbe68d6a9c8a8cb6fca245c2321dbcd2b038293
vitorcontiz <vitorcontizgit@gmail.com>
2024-02-22 13:43:30 -0300

    feat: ensure AccountMongoRepository returns an account on success

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb'
+import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: null as unknown as MongoClient,
@@ -12,5 +12,8 @@ export const MongoHelper = {
     if (this.client) {
       await this.client.close()
     }
+  },
+  getCollection (name: string): Collection {
+    return this.client.db().collection('name')
   }
 }
