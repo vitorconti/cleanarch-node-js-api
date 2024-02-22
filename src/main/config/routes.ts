c7dcb5b5f63c55bb8c79e668bfe2f4c16b5f2c26
vitorcontiz <vitorcontizgit@gmail.com>
2024-02-22 16:49:11 -0300

    feat: ensure signup route returns an account on success

---
+++ src/main/config/routes.ts
@@ -0,0 +1,12 @@
+import { Express, Router } from 'express'
+import { readdirSync } from 'fs'
+import { join } from 'path'
+export default (app: Express): void => {
+  const router = Router()
+  app.use('/api', router)
+  readdirSync(join(__dirname, '../routes')).map(async file => {
+    if (!file.endsWith('test.ts')) {
+      (await import(`../routes/${file}`)).default(router)
+    }
+  })
+}
