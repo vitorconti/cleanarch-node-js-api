3477acdf0cdae52f88f02eee6ecd5c7797d135d3
vitorcontiz <vitorcontizgit@gmail.com>
2024-02-27 17:48:53 -0300

    refactor: move required field validation to the validation composite

--- src/main/factories/signup-validation.spec.ts
+++ src/main/factories/signup-validation.spec.ts
@@ -1,14 +1,15 @@
 import { makeSignUpValidation } from './signup-validation'
 import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite'
 import { RequiredFieldValidation } from '../../presentation/helpers/validators/required-field-validation'
+import { Validation } from '../../presentation/helpers/validators/validation'
 
 jest.mock('../../presentation/helpers/validators/validation-composite')
 
 describe('SignUpValidation Factory', () => {
   test('Should call ValidationComposite with all validation', () => {
     makeSignUpValidation()
-    expect(ValidationComposite).toHaveBeenCalledWith([
-      new RequiredFieldValidation('name')
-    ])
+    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+    const validations: Validation[] = requiredFields.map(requiredField => new RequiredFieldValidation(requiredField))
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
   })
 })
--- src/main/factories/signup-validation.ts
+++ src/main/factories/signup-validation.ts
@@ -1,8 +1,9 @@
 import { RequiredFieldValidation } from '../../presentation/helpers/validators/required-field-validation'
+import { Validation } from '../../presentation/helpers/validators/validation'
 import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite'
 
 export const makeSignUpValidation = (): ValidationComposite => {
-  return new ValidationComposite([
-    new RequiredFieldValidation('name')
-  ])
+  const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+  const validations: Validation[] = requiredFields.map(requiredField => new RequiredFieldValidation(requiredField))
+  return new ValidationComposite(validations)
 }
