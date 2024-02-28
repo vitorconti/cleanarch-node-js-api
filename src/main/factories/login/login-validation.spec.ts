b962ae3b5212ac68e13034c4ed198c8301278246
vitorcontiz <vitorcontizgit@gmail.com>
2024-02-28 15:36:12 -0300

    refactor: group validation imports in a single file

--- src/main/factories/login/login-validation.spec.ts
+++ src/main/factories/login/login-validation.spec.ts
@@ -1,9 +1,7 @@
-import { makeLoginValidation } from './login-validation'
-import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite'
-import { RequiredFieldValidation } from '../../../presentation/helpers/validators/required-field-validation'
-import { Validation } from '../../../presentation/protocols/validation'
+import { EmailValidation, RequiredFieldValidation, ValidationComposite } from '../../../presentation/helpers/validators'
 import { EmailValidator } from '../../../presentation/protocols/email-validator'
-import { EmailValidation } from '../../../presentation/helpers/validators/email-validation'
+import { Validation } from '../../../presentation/protocols/validation'
+import { makeLoginValidation } from './login-validation'
 
 jest.mock('../../../presentation/helpers/validators/validation-composite')
 
--- src/main/factories/signup/signup-validation.spec.ts
+++ src/main/factories/signup/signup-validation.spec.ts
@@ -1,10 +1,7 @@
-import { makeSignUpValidation } from './signup-validation'
-import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite'
-import { RequiredFieldValidation } from '../../../presentation/helpers/validators/required-field-validation'
-import { Validation } from '../../../presentation/protocols/validation'
-import { CompareFieldsValidation } from '../../../presentation/helpers/validators/compare-fields-validation'
+import { CompareFieldsValidation, EmailValidation, RequiredFieldValidation, ValidationComposite } from '../../../presentation/helpers/validators'
 import { EmailValidator } from '../../../presentation/protocols/email-validator'
-import { EmailValidation } from '../../../presentation/helpers/validators/email-validation'
+import { Validation } from '../../../presentation/protocols/validation'
+import { makeSignUpValidation } from './signup-validation'
 
 jest.mock('../../../presentation/helpers/validators/validation-composite')
 
