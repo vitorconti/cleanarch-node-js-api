a904882c1470d7d0af39b5c245b4b93bcf4ea6d0
vitorcontiz <vitorcontizgit@gmail.com>
2024-02-28 08:50:10 -0300

    refactor: move password comparation to the ValidationComposite

--- src/main/factories/signup-validation.spec.ts
+++ src/main/factories/signup-validation.spec.ts
@@ -2,6 +2,7 @@ import { makeSignUpValidation } from './signup-validation'
 import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite'
 import { RequiredFieldValidation } from '../../presentation/helpers/validators/required-field-validation'
 import { Validation } from '../../presentation/helpers/validators/validation'
+import { CompareFieldsValidation } from '../../presentation/helpers/validators/compare-fields-validation'
 
 jest.mock('../../presentation/helpers/validators/validation-composite')
 
@@ -10,6 +11,7 @@ describe('SignUpValidation Factory', () => {
     makeSignUpValidation()
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     const validations: Validation[] = requiredFields.map(requiredField => new RequiredFieldValidation(requiredField))
+    validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
     expect(ValidationComposite).toHaveBeenCalledWith(validations)
   })
 })
--- src/main/factories/signup-validation.ts
+++ src/main/factories/signup-validation.ts
@@ -1,3 +1,4 @@
+import { CompareFieldsValidation } from '../../presentation/helpers/validators/compare-fields-validation'
 import { RequiredFieldValidation } from '../../presentation/helpers/validators/required-field-validation'
 import { Validation } from '../../presentation/helpers/validators/validation'
 import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite'
@@ -5,5 +6,6 @@ import { ValidationComposite } from '../../presentation/helpers/validators/valid
 export const makeSignUpValidation = (): ValidationComposite => {
   const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
   const validations: Validation[] = requiredFields.map(requiredField => new RequiredFieldValidation(requiredField))
+  validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
   return new ValidationComposite(validations)
 }
