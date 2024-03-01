b66a19afe85204b4687970ddcf0ceb9df95519d4
vitorcontiz <vitorcontizgit@gmail.com>
2024-03-01 14:50:48 -0300

    feat: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,14 +1,16 @@
 import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
-
+import { Collection } from 'mongodb'
+import { hash } from 'bcrypt'
+let accountCollection: Collection
 describe('Login Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL ?? '')
   })
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
@@ -25,6 +27,23 @@ describe('Login Routes', () => {
           email: 'joseconti@teste.com',
           password: 'teste',
           passwordConfirmation: 'teste'
+        })
+        .expect(200)
+    })
+  })
+  describe('/POST /login', () => {
+    test('Should return 200 on login', async () => {
+      const password = await hash('teste', 12)
+      await accountCollection.insertOne({
+        name: 'jose',
+        email: 'joseconti@teste.com',
+        password
+      })
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'joseconti@teste.com',
+          password: 'teste'
         })
         .expect(200)
     })
