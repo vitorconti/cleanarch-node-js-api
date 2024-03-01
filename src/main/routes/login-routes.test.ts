4ba0c47f02b6c2a8b9bbcd12f4d5af952e0303fb
vitorcontiz <vitorcontizgit@gmail.com>
2024-03-01 14:54:19 -0300

    test: ensure login route returns 401 if a invalid account is provided

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -47,5 +47,14 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+    test('Should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'joseconti@teste.com',
+          password: 'teste'
+        })
+        .expect(401)
+    })
   })
 })
