2f98babcefa44d586e996d330dff5f431c9e9f46
vitorcontiz <vitorcontizgit@gmail.com>
2024-03-01 11:13:12 -0300

    feat: ensure AccountMongoRepository returns null if loadByemail fails

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -14,6 +14,6 @@ export class AccountMongoRepository implements AddAccountRepository, LoadAccount
   async loadByEmail (email: string): Promise<AccountModel | null> {
     const accountCollection = await MongoHelper.getCollection('accounts')
     const account = await accountCollection.findOne({ email })
-    return MongoHelper.map<AccountModel>(account)
+    return account && MongoHelper.map<AccountModel>(account)
   }
 }
