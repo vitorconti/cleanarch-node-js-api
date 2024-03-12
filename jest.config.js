7261467d60b95218ca4938c1141cffc7c578ff7d
vitorcontiz <vitorcontizgit@gmail.com>
2024-03-12 13:58:56 -0300

    refactor: add files at jest ignore list

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!**/env.ts', '!**/http-helper.ts', '!**/server.ts', '!**/index.ts', '!**/*protocols.ts'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',
