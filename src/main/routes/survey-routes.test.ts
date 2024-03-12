e1eb19d0f517d5be0b12e272ed87fec9c404067e
vitorcontiz <vitorcontizgit@gmail.com>
2024-03-12 15:56:53 -0300

    test: ensure load surveys route returns 200 if a valid token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -61,5 +61,25 @@ describe('Login Routes', () => {
         .get('/api/surveys')
         .expect(403)
     })
+    test('Should return 204 on load surveys with valid accessToken', async () => {
+      const res = await accountCollection.insertOne({
+        name: 'jose',
+        email: 'joseconti@teste.com',
+        password: 123
+      })
+      const id = res.ops[0]._id
+      const accessToken = sign({ id }, env.jwtSecret)
+      await accountCollection.updateOne({
+        _id: id
+      }, {
+        $set: {
+          accessToken
+        }
+      })
+      await request(app)
+        .get('/api/surveys')
+        .set('x-access-token', accessToken)
+        .expect(204)
+    })
   })
 })
